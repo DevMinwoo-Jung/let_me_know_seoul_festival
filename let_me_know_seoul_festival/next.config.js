9cea62bc3272058acb185feb23827bacc4a44115
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-11-04 15:16:23 +0900

    sibal

--- let_me_know_seoul_festival/next.config.js
+++ let_me_know_seoul_festival/next.config.js
@@ -11,24 +11,4 @@ module.exports = {
     unoptimized: true,
   },
   assetPrefix: "http://letmeknowseoulfestival.life/", // HTTP로 변경
-  async redirects() {
-    return [
-      {
-        source: '/secure/:path*', // 예를 들어, 특정 경로에서만 HTTPS를 사용하도록 설정
-        has: [
-          {
-            type: 'host',
-            value: 'letmeknowseoulfestival.life',
-          },
-          {
-            type: 'header',
-            key: 'x-forwarded-proto',
-            value: 'http',
-          },
-        ],
-        destination: 'https://letmeknowseoulfestival.life/secure/:path*', // 특정 경로만 HTTPS로 리디렉션
-        permanent: true,
-      },
-    ];
-  },
 };
