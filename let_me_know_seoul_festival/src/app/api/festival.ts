e4354b1a5406f570e0f1858522f820edfc008e92
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-11-04 10:26:42 +0900

    asdasd

--- let_me_know_seoul_festival/src/app/api/festival.ts
+++ let_me_know_seoul_festival/src/app/api/festival.ts
@@ -1,4 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { SERVICEKEY,festivalEndPoint } from './festivalEndPoint'
+
 
 interface getFestivalPerPageI {
   start: string;
@@ -9,18 +11,16 @@ interface getFestivalPerPageI {
 }
 
 
-
 export const festivalAPI = createApi({
   reducerPath: 'festivalAPI',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api/proxy' }), // 프록시 API 경로로 설정
+  baseQuery: fetchBaseQuery({ baseUrl: `${festivalEndPoint}/${SERVICEKEY}/json/culturalEventInfo` }),
   tagTypes: [],
   endpoints: (builder) => ({
     getFestivalPerPage: builder.query({
-      query: ({ start, end, codeName = '', title = '', date = '' }: getFestivalPerPageI) =>
-        `?start=${start}&end=${end}&codeName=${codeName}&title=${title}&date=${date}`,
+      query: ({start, end, codeName, title, date }:getFestivalPerPageI) => `/${start}/${end}/${codeName}/${title}/${date}`,
     }),
   }),
-});
+})
 
 // Export hooks for usage in functional components
-export const { useGetFestivalPerPageQuery } = festivalAPI; 
+export const { useGetFestivalPerPageQuery } = festivalAPI;
