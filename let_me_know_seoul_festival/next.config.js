3f03a47dd322963dba6823e2bff6fa806813c6c2
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-11-04 10:46:01 +0900

    asdasd

--- let_me_know_seoul_festival/next.config.js
+++ let_me_know_seoul_festival/next.config.js
@@ -18,7 +18,7 @@ module.exports = {
     return [
       {
         source: '/(.*)',
-        destination: 'http://letmeknowseoulfestival.life/$1',
+        destination: 'https://letmeknowseoulfestival.life/$1',
         permanent: true,
       },
     ];
