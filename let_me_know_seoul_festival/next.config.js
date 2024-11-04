5202132ff965f5a3b57c4d529995895b626a6ba7
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-11-04 15:26:52 +0900

    asd?

--- let_me_know_seoul_festival/next.config.js
+++ let_me_know_seoul_festival/next.config.js
@@ -11,4 +11,12 @@ module.exports = {
     unoptimized: true,
   },
   assetPrefix: "http://letmeknowseoulfestival.life/", // HTTP로 변경
+  async redirects() {
+    return [
+      {
+        destination: 'https://letmeknowseoulfestival.life/$1',
+        permanent: true,
+      }
+    ]
+  }
 };
