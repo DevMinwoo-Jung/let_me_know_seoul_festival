6c151190b659b39cabccdcc4659c512d755de47f
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-11-04 09:13:40 +0900

    sadasd

--- let_me_know_seoul_festival/src/app/API/proxy.js
+++ let_me_know_seoul_festival/src/app/API/proxy.js
@@ -1,6 +1,8 @@
+import { SERVICEKEY } from './festivalEndPoint'
+
 export default async function handler(req, res) {
   try {
-    const response = await fetch("http://openapi.seoul.go.kr:8088/");
+    const response = await fetch(`http://openapi.seoul.go.kr:8088/48504d46446d696e373365494c7848/json/culturalEventInfo`);
     
     if (!response.ok) {
       throw new Error('Failed to fetch data');
