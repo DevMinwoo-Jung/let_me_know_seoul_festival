5cd86c5357f237721d9c8fa8018ea1cf29b8b723
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-11-04 23:19:06 +0900

    proxy at next.config.js

--- let_me_know_seoul_festival/next.config.js
+++ let_me_know_seoul_festival/next.config.js
@@ -15,18 +15,10 @@ module.exports = {
   },
   assetPrefix: "https://letmeknowseoulfestival.life/",
   async rewrites() {
-    return [ 
-      {
-        source: '/api/proxy/:path*',
-        destination: 'http://openapi.seoul.go.kr:8088/:path*', // Proxy API requests
-      },
-    ];
-  },
-  async rewrites() {
     return [
       {
-        source: "/api/proxy",
-        destination: `http://openapi.seoul.go.kr:8088/48504d46446d696e373365494c7848/json/culturalEventInfo`,
+        source: '/api/proxy/:path*',
+        destination: 'http://openapi.seoul.go.kr:8088/48504d46446d696e373365494c7848/json/culturalEventInfo/:path*',
       },
     ];
   },
