5801f83d81513c65904f7c1bd832c7b902ef5cf3
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-11-05 22:58:39 +0900

    config

--- let_me_know_seoul_festival/next.config.js
+++ let_me_know_seoul_festival/next.config.js
@@ -13,7 +13,7 @@ module.exports = {
     ],
     unoptimized: true,
   },
-  
+  productionBrowserSourceMaps: false,
   assetPrefix: process.env.NODE_ENV === "production" ? "https://letmeknowseoulfestival.life/" : "",
   async rewrites() {
     return [
