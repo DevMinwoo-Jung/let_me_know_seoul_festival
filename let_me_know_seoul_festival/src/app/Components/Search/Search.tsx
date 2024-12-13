195b4a78e9e813953187305e6e550a9f4fe072ca
DEVMINWOO-JUNG <applejung312@gmail.com>
2024-12-13 12:15:33 +0900

    왜 안되지

--- let_me_know_seoul_festival/src/app/Components/Search/Search.tsx
+++ let_me_know_seoul_festival/src/app/Components/Search/Search.tsx
@@ -51,8 +51,6 @@ export default function Search() {
   const activeEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
 
     let enterKeyCode = getKeyCodeByUserAgnet();
-    alert((e.target as HTMLInputElement).value)
-    alert(enterKeyCode)
     if (Number(e.code) === enterKeyCode || e.code === 'Enter') {
       setIsSubmit(true);
       dispatch(setTitle({
